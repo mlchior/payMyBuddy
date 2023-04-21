90af4438813393a3aa1695c5dd1fd6d1f49da827
Melchior Crinier <66031896+mlchior@users.noreply.github.com>
2023-04-21 18:29:59 +0200

    21 avril

--- src/main/resources/paymybuddy.sql
+++ src/main/resources/paymybuddy.sql
@@ -1,38 +1,64 @@
-
-
-DROP TABLE IF EXISTS user;
-CREATE TABLE user (
-                        id INT PRIMARY KEY AUTO_INCREMENT,
-                        adresse_email VARCHAR(255) UNIQUE NOT NULL,
-                        mot_de_passe VARCHAR(255) NOT NULL,
-                        username VARCHAR(255) NOT NULL
-);
-DROP TABLE IF EXISTS compte;
-CREATE TABLE compte (
-                        id INT PRIMARY KEY AUTO_INCREMENT,
-                        id_user INT NOT NULL,
-                        solde FLOAT NOT NULL,
-                        FOREIGN KEY (id_user) REFERENCES user(id)
-
+create table user
+(
+    id_user       int auto_increment
+        primary key,
+    adresse_email varchar(255) not null,
+    mot_de_passe  varchar(255) not null,
+    username      varchar(255) not null,
+    id            int          not null,
+    constraint adresse_email
+        unique (adresse_email)
 );
 
-DROP TABLE IF EXISTS friend;
-CREATE TABLE friend (
-                        id INT PRIMARY KEY AUTO_INCREMENT,
-                        id_user INT NOT NULL,
-                        adresse_email_friend VARCHAR(255) UNIQUE NOT NULL,
-                        id_user_friend INT NOT NULL,
-                        FOREIGN KEY (id_user) REFERENCES user(id)
-
+create table compte
+(
+    id_compte int auto_increment
+        primary key,
+    id_user   int   not null,
+    solde     float not null,
+    id        int   not null,
+    constraint compte_ibfk_1
+        foreign key (id_user) references user (id_user)
 );
 
-DROP TABLE IF EXISTS transaction;
-CREATE TABLE transaction (
-                        id INT PRIMARY KEY AUTO_INCREMENT,
-                        id_user INT NOT NULL,
-                        id_user_reception INT NOT NULL,
-                        montant FLOAT NOT NULL,
-                        date_time DATETIME NOT NULL,
-                        FOREIGN KEY (id_user) REFERENCES user(id),
-                        FOREIGN KEY (id_user_reception) REFERENCES friend(id)
+create index id_user
+    on compte (id_user);
+
+create table friend
+(
+    id_friend      int auto_increment
+        primary key,
+    id_user        int not null,
+    id_user_friend int null,
+    id             int not null,
+    constraint FKboxgsy789akxcdj6vbssujyna
+        foreign key (id_user_friend) references user (id_user),
+    constraint friend_ibfk_1
+        foreign key (id_user) references user (id_user)
 );
+
+create index id_user
+    on friend (id_user);
+
+create table transaction
+(
+    id_transaction    int auto_increment
+        primary key,
+    id_user           int          not null,
+    id_user_reception int          not null,
+    montant           float        not null,
+    date_time         datetime     not null,
+    frais             int          null,
+    description       varchar(255) null,
+    id                int          not null,
+    constraint transaction_ibfk_1
+        foreign key (id_user) references user (id_user),
+    constraint transaction_ibfk_2
+        foreign key (id_user_reception) references friend (id_friend),
+    constraint FKipnt7wb0eyeegoo1pyqyapamq
+        foreign key (id_user_reception) references user (id_user)
+);
+
+create index id_user
+    on transaction (id_user);
+
